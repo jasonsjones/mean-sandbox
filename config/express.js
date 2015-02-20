4caabd7dd521f8fd2b56b5ca7b4c52cf8a5e42dd
Jason Jones <jsjones96@gmail.com>
2015-02-20 18:42:03 +0000

    fix for loading static content

--- config/express.js
+++ config/express.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var session = require('express-session');
@@ -11,7 +12,8 @@ function logger(req, res, next) {
 module.exports = function (config) {
     var app = express();
 
-    app.use(express.static(__dirname + '/../public/'));
+    app.use(express.static(path.join(__dirname + '/../public')));
+    app.use('components', express.static(path.join(__dirname + '/../public/components')));
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
 
