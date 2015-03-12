c1fd18e05f643e39b1fa445d661fd947a0f90ea4
Jason Jones <jsjones96@gmail.com>
2015-03-12 23:21:37 +0000

    add morgan for server-side logging

--- config/express.js
+++ config/express.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 var passport = require('passport');
 var session = require('express-session');
 
-function logger(req, res, next) {
-    console.log(req.method + ' ' + req.url + ' -- ' + res.statusCode);
-    next();
-}
+// function logger(req, res, next) {
+//     console.log(req.method + ' ' + req.url + ' -- ' + res.statusCode);
+//     next();
+// add another comment
+// }
 
 module.exports = function (config) {
     var app = express();
     var router = express.Router();
 
-    app.use(logger);
+    app.use(logger('dev'));
     app.use('components', express.static(path.join(__dirname + '/../public/components')));
     app.use(express.static(path.join(__dirname + '/../public')));
     app.use(express.static(path.join(__dirname + '/../')));
