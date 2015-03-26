e4a9623613360ef60249b9d0624f37a005ae4e30
Jason S. Jones <jsjones96@gmail.com>
2015-03-26 05:41:31 -0700

    minor strng edit

--- public/app/main/dataservice.js
+++ public/app/main/dataservice.js
@@ -27,7 +27,7 @@
             {imgUrl: 'img/nodejs.png',
              webUrl: 'http://nodejs.org',
              description: 'Node.js is a platform built on Chrome\'s JavaScript runtime ' +
-                 'for easily building fast, scalable network applications.'
+                 'for easily building fast, scalable network applications'
             }
 
         ];
