09d09202c158d8fd9c2d849d09c0d3553ef2de44
Jason Jones <jsjones96@gmail.com>
2015-06-03 20:57:19 +0000

    remove dummy data and add 'get' api call

--- public/app/atm/atm.js
+++ public/app/atm/atm.js
@@ -7,63 +7,18 @@
         .factory('ATM', ATM);
 
     //////////////
-    function ATM() {
-        var data = [
-            {
-              "idx" : 0,
-              "cashAmount" : 40,
-              "serviceFee" : 3,
-              "date" : "6/10/2014",
-              "purchases" : [
-                {"amount": 3, "description": "ATM service fee"},
-                {"amount": 5, "description": "lunch"},
-                {"amount": 2, "description": "shaving cream"},
-                {"amount": 2, "description": "groceries"},
-                {"amount": 6, "description": "coffee dues"},
-                {"amount": 6, "description": "lunch"},
-                {"amount": 11, "description": "lunch"},
-                {"amount": 1, "description": "tip"},
-                {"amount": 5, "description": "lunch"}
-              ]
-            },
-            {
-            "idx" : 1,
-            "cashAmount": 60,
-            "serviceFee": 3,
-            "date" : "6/3/2014",
-            "purchases" : [
-                {"amount": 3, "description": "ATM service fee"},
-                {"amount": 5, "description": "lunch"},
-                {"amount": 6, "description": "coffee dues"}
-            ]
-        },
-            {
-            "idx" : 2,
-            "cashAmount": 80,
-            "serviceFee": 2,
-            "date" : "7/3/2014",
-            "purchases" : [
-                {"amount": 2, "description": "ATM service fee"},
-                {"amount": 5, "description": "lunch"},
-                {"amount": 20, "description": "groceries"},
-                {"amount": 6, "description": "coffee dues"},
-                {"amount": 6, "description": "dinner"}
-            ]
-        }
-        ];
+    function ATM($http) {
 
         var service = {
-            getTransactions: getTransactions,
-            getTotalAmount: getTotalAmount
+            getTotalAmount: getTotalAmount,
+            get: function () {
+                return $http.get('/api/atms');
+            }
         };
 
         return service;
 
         ////////////////////////
-        function getTransactions() {
-            return data;
-        }
-
         function getTotalAmount(transaction) {
             return transaction.cashAmount + transaction.serviceFee;
         }
