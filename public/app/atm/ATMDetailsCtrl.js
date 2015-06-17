8af2b3ef205eae64a2d2e91c213929448561747b
Jason S. Jones <jsjones96@gmail.com>
2015-06-17 06:10:23 -0700

    edit purchase on atm-details page -- wip

--- public/app/atm/ATMDetailsCtrl.js
+++ public/app/atm/ATMDetailsCtrl.js
@@ -3,21 +3,43 @@
     angular.module('app.atm')
         .controller('ATMDetailsCtrl', ATMDetailsCtrl);
 
-    ////////////
     function ATMDetailsCtrl($routeParams, ATM, purchase) {
         var vm = this;
+        var id = $routeParams.atmId;
+
         vm.loading = true;
+        vm.editPurchaseState = false;
         vm.purchases = null;
         vm.totalSpent = 0;
         vm.totalAmount = 0;
-        var id = $routeParams.atmId;
 
-        vm.addPurchase = function () {
-            var newPurchase = {
+        vm.newPurchase = {};
+
+        vm.addPurchase = addPurchase;
+        vm.deletePurchase = deletePurchase;
+        vm.getTotalSpent = getTotalSpent;
+        vm.getPurchases = getPurchases;
+        vm.editPurchase = editPurchase;
+        vm.updatePurchase = updatePurchase;
+
+        initialize();
+
+        /*********** Implementation Details ************/
+        function initialize() {
+            ATM.getById(id).then(function (data) {
+                vm.transaction = data;
+                vm.totalAmount = ATM.getTotalAmount(vm.transaction);
+            });
+
+            vm.getPurchases();
+        }
+
+        function addPurchase () {
+            var newPurchaseData = {
                 amount: vm.newPurchase.amount,
                 description: vm.newPurchase.description
             };
-            purchase.add(newPurchase, id).then(function (data) {
+            purchase.add(newPurchaseData, id).then(function (data) {
                 vm.purchases.push(data);
                 vm.totalSpent = vm.getTotalSpent();
             }, function (reason) {
@@ -25,9 +47,9 @@
             });
             vm.newPurchase.amount = '';
             vm.newPurchase.description = '';
-        };
+        }
 
-        vm.deletePurchase = function (purchaseToDelete) {
+        function deletePurchase(purchaseToDelete) {
             purchase.remove(id, purchaseToDelete._id).then(function (result) {
                 if (result.success) {
                     console.log('successfully deleted');
@@ -38,32 +60,28 @@
             }, function () {
                 console.log('error deleting purchase');
             });
-        };
+        }
 
-        vm.getTotalSpent = function () {
+        function getTotalSpent() {
             return vm.purchases.reduce(function (prev, curr) {
                 return prev + curr.amount;
             }, 0);
-        };
+        }
 
-        vm.getPurchases = function () {
+        function getPurchases() {
             purchase.get(id).then(function (data) {
                 vm.purchases = data;
                 vm.loading = false;
                 vm.totalSpent = vm.getTotalSpent();
             });
-        };
+        }
 
-        initialize();
-        function initialize() {
-            ATM.getById(id).then(function (data) {
-                vm.transaction = data;
-                vm.loading = false;
-                vm.totalAmount = ATM.getTotalAmount(vm.transaction);
-            });
+        function editPurchase() {
+            vm.editPurchaseState = true;
+        }
 
-            vm.getPurchases();
-
+        function updatePurchase() {
+            vm.editPurchaseState = false;
         }
     }
 })();
