8bc49d6e00d25ab4fe058cda53c75b18d25f7b2e
Jason Jones <jsjones96@gmail.com>
2015-06-28 04:16:03 +0000

    add testing skeleton for profile ctrl

---
+++ test/unit/account/profileCtrlSpec.js
@@ -0,0 +1,54 @@
+'use strict';
+
+describe('ProfileCtrl', function () {
+
+    beforeEach(module('app.account'))
+    beforeEach(function () {
+        module('app.core');
+    });
+
+    var regService, sandbox;
+
+    var profileCtrl,
+        mockNotifier,
+        mockLocation,
+        mockIdentity;
+
+    beforeEach(inject(function ($controller, $injector) {
+
+        sandbox = sinon.sandbox.create();
+        regService = $injector.get('register');
+
+        mockNotifier = {
+            notify: function () {}
+        },
+
+        mockIdentity = {
+            currentUser: {
+                local: {
+                    username: 'larry'
+                }
+            },
+            isAuthenticated: function () { },
+            isAuthorizedForRole: function (role) { }
+        };
+
+        mockLocation = {
+            path: function (url) { }
+        };
+
+        profileCtrl = $controller('ProfileCtrl', {
+            $location: mockLocation,
+            identity: mockIdentity,
+            notifier: mockNotifier
+        });
+    }));
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    it('is defined', function () {
+       expect(profileCtrl).to.exist;
+    });
+});
