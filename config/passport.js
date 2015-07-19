41686bfb3fb0780bc5ebd76ff6b812696cde0fb9
Jason S. Jones <jsjones96@gmail.com>
2015-07-19 08:07:04 -0700

    change deserializeUser -- don't send salt or pwd

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ module.exports = function() {
     });
 
     passport.deserializeUser(function (id, done) {
-        User.findById(id, function (err, user) {
+        User.findOne({_id: id}, '-salt -password', function (err, user) {
             done(err, user);
         });
     });
