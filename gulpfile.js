3539eb9bb018fd2aa356b4d0151793d87bd0312b
Jason S. Jones <jsjones96@gmail.com>
2015-08-01 20:27:47 -0700

    add static asset rev to optimize pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -139,8 +139,12 @@ gulp.task('optimize', ['inject', 'fonts', 'images'], function () {
         .pipe($.ngAnnotate())
         .pipe($.uglify())
         .pipe(jsAppFilter.restore())
+        .pipe($.rev())
         .pipe(assets.restore())
         .pipe($.useref())
+        .pipe($.revReplace())
+        .pipe(gulp.dest(config.build))
+        .pipe($.rev.manifest())
         .pipe(gulp.dest(config.build));
 });
 
