fab6aef807d1b256963f720a5ade81ad7ff285af
Jason S. Jones <jsjones96@gmail.com>
2015-08-14 06:31:25 -0700

    Change user details page view/UI

--- public/app/admin/UserDetailsCtrl.js
+++ public/app/admin/UserDetailsCtrl.js
@@ -1,12 +1,25 @@
 (function () {
     'use strict';
+
     angular.module('app.core')
         .controller('UserDetailsCtrl', UserDetailsCtrl);
 
     function UserDetailsCtrl($routeParams, userCache) {
 
         var vm = this;
+        vm.userTableSortCol = '-roles';
 
         vm.user = userCache.getUserById($routeParams.userId);
+        vm.isAdmin = isAdmin;
+
+        /**************** Implementation Details *******************/
+        function isAdmin(user) {
+            if (user) {
+                return user.roles.indexOf('admin') > -1;
+            } else {
+                return false;
+            }
+        }
+
     }
 }());
