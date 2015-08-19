25073bd8663a9f19e6cc0ec1f622a782f5f89c17
Jason Jones <jsjones96@gmail.com>
2015-08-19 18:13:57 +0000

    Change callbackURL to c9.io

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     "twitterAuth": {
         "consumerKey": process.env.SB_TWITTER_CONSUMER_KEY,
         "consumerSecret": process.env.SB_TWITTER_CONSUMER_SECRET,
-        "callbackURL": "http://local.meansandbox.com:3000/auth/twitter/callback"
+        "callbackURL": "https://mean-sandbox-jsj0nes.c9.io/auth/twitter/callback"
     }
 };
