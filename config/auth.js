95c8c088843da3819957d0ad83e56a692dbd6b34
Jason S. Jones <jsjones96@gmail.com>
2015-08-20 06:19:20 -0700

    Change twitter oauth callback to localhost

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     "twitterAuth": {
         "consumerKey": process.env.SB_TWITTER_CONSUMER_KEY,
         "consumerSecret": process.env.SB_TWITTER_CONSUMER_SECRET,
-        "callbackURL": "https://mean-sandbox-jsj0nes.c9.io/auth/twitter/callback"
+        "callbackURL": "http://127.0.0.1:3000/auth/twitter/callback"
     }
 };
