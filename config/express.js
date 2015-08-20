be0a685f9941a7661a67a3a8c389c41207c8b6eb
Jason S. Jones <jsjones96@gmail.com>
2015-08-19 19:34:07 -0700

    Change setHeaders in middleware

--- config/express.js
+++ config/express.js
@@ -23,9 +23,10 @@ module.exports = function (config) {
     app.use(passport.session());
 
     app.use(function (req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.setHeader('Access-Control-Allow-Credentials', true);
         next();
     });
 
