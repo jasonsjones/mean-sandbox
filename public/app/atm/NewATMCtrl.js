b34143e295e3aeab1b46aa6010197b455eb9a867
Jason S. Jones <jsjones96@gmail.com>
2015-09-24 07:01:10 -0700

    Add input validation

--- public/app/atm/NewATMCtrl.js
+++ public/app/atm/NewATMCtrl.js
@@ -3,8 +3,8 @@
     angular.module('app.atm')
         .controller('NewATMCtrl', NewATMCtrl);
 
-    NewATMCtrl.$inject = ['$location', 'ATM'];
-    function NewATMCtrl($location, ATM) {
+    NewATMCtrl.$inject = ['$location', 'ATM', 'notifier'];
+    function NewATMCtrl($location, ATM, notifier) {
         var vm = this;
 
         vm.add = add;
@@ -18,9 +18,26 @@
                 location: vm.location
             };
 
-            ATM.addTransaction(newATMData).then(function (data) {
-                $location.path('/atm');
-            });
+            if (!newATMData.serviceFee || newATMData < 0) {
+                newATMData.serviceFee = 0;
+            }
+
+            if (inputsValid(newATMData)) {
+                ATM.addTransaction(newATMData).then(function (data) {
+                    $location.path('/atm');
+                });
+            } else {
+                notifier.error('There is something wrong with the data you entered');
+            }
+        }
+
+        function inputsValid(data) {
+
+            if (data.cashAmount > 0 && data.date && data.location) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 })();
