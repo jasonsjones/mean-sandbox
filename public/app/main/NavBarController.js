7a0044503e845360f00ca749ea8880c368e64129
Jason Jones <jsjones96@gmail.com>
2015-09-29 19:31:47 +0000

    Rename nav bar controller iaw styleguide

--- public/app/main/NavBarController.js
+++ public/app/main/NavBarController.js
@@ -1,16 +1,18 @@
 (function () {
     'use strict';
-    angular.module('app.core')
-        .controller('NavBarCtrl', NavBarCtrl);
+
+    angular
+        .module('app.core')
+        .controller('NavBarController', NavBarController);
 
     NavBarCtrl.$inject = ['$location', 'identity'];
-    function NavBarCtrl($location, identity) {
+    function NavBarController($location, identity) {
 
         var vm = this;
         vm.identity = identity;
         vm.isActive = isActive;
 
-        /********* Implementation Details **********/
+        /********* implementation details **********/
         function isActive(viewLocation) {
             return viewLocation === $location.path();
         }
