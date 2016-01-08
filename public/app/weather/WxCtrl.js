01dd2a44a5568908733544589dc2195ff734364a
Jason Jones <jsjones96@gmail.com>
2016-01-08 00:15:15 +0000

    Add skeleton for zipcode 3rd party api

--- public/app/weather/WxCtrl.js
+++ public/app/weather/WxCtrl.js
@@ -11,8 +11,10 @@
         vm.zipcode = '98125';
         vm.loading = true;
         vm.wx = null;
+        vm.cityInfo = null;
 
         vm.getWxByZip = getWxByZip;
+        vm.getInfoByZip = getInfoByZip;
 
         $scope.$on('userChange', function (data) {
             wxService.clearCache();
@@ -35,11 +37,22 @@
                 });
         }
 
+        function getInfoByZip(zip) {
+            vm.loading = true;
+            wxService.getInfoByZip(zip)
+                .then(function(data) {
+                    vm.cityInfo = data;
+                    vm.loading = false;
+                    console.log(vm.cityInfo);
+                });
+        }
+
         function primeWxInfo() {
             if (identity.currentUser) {
                 getWxByZip(identity.currentUser.zipcode);
             } else {
-                getWxByZip(vm.zipcode);
+                // getWxByZip(vm.zipcode);
+                getInfoByZip(vm.zipcode);
             }
         }
 
--- public/app/weather/wxService.js
+++ public/app/weather/wxService.js
@@ -7,9 +7,11 @@
     wxService.$inject = ['$http', '$q'];
     function wxService($http, $q) {
         var wxCache = null;
+        var cityInfoCache = null;
 
         var factory = {
             getWxByZip: getWxByZip,
+            getInfoByZip: getInfoByZip,
             clearCache: clearCache
         };
 
@@ -23,7 +25,7 @@
             } else {
                 var baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
                 var query = '?zip=' + zip + ',us&units=imperial';
-                var API = '&APPID=a600be65b6ec16f86cf386aefd894300'
+                var API = '&APPID=a600be65b6ec16f86cf386aefd894300';
                 var url = baseUrl + query + API;
 
                 $http.get(url)
@@ -39,8 +41,26 @@
 
         }
 
+        function getInfoByZip(zip) {
+            var deferred = $q.defer();
+            if (cityInfoCache) {
+                deferred.resolve(cityInfoCache);
+            } else {
+                var key = 'pPRkpWH11B8ZiypsBuFNFR6DwwvcVZIkPzFevmgxXoxgXWIzqnm6etJL0fXVfGf4';
+                var baseUrl = 'https://www.zipcodeapi.com/rest/' + key;
+                var query = '/info.json/' + zip + '/degrees';
+                var url = baseUrl + query;
+                console.log(url);
+
+                // call $http.get(url) to get data to pass to the resolve method
+                deferred.resolve({city: 'Seattle', state: 'WA'});
+            }
+            return deferred.promise;
+        }
+
         function clearCache() {
             wxCache = null;
+            cityInfoCache = null;
         }
     }
 }());
