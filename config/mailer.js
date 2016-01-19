b92f0b39838f2e1814c5642256aec4f5526e5571
Jason S. Jones <jsjones96@gmail.com>
2016-01-19 06:06:16 -0800

    Change config for mail transport

--- config/mailer.js
+++ config/mailer.js
@@ -2,13 +2,17 @@ var nodemailer = require('nodemailer');
 
 module.exports = function () {
 
-    var transport = nodemailer.createTransport({
-        service: 'Gmail',
+    var smtpConfig = {
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.MS_GMAIL_USER,
             pass: process.env.MS_GMAIL_PASS
         }
-    });
+    };
+
+    var transport = nodemailer.createTransport(smtpConfig);
 
     return transport;
 };
